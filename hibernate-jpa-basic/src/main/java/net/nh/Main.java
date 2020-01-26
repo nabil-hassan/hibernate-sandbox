b726c8654995556a81952d7b84e400e0ab93ce2b
Nabil Hassan <nabil.a.hassan@gmail.com>
2020-01-26 21:57:48 +0000

    Starting work on JPA basic project

---
+++ hibernate-jpa-basic/src/main/java/net/nh/Main.java
@@ -0,0 +1,15 @@
+package net.nh;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+
+    private static final Logger LOG = LoggerFactory.getLogger(Main.class);
+
+    public static void main (String[] args) {
+        LOG.info("Starting application");
+    }
+
+
+}
---
+++ hibernate-jpa-basic/src/main/java/net/nh/entity/Message.java
@@ -0,0 +1,47 @@
+package net.nh.entity;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "message")
+public class Message {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
+    private Long id;
+
+    private String text;
+
+    private Message nextMessage;
+
+    public Long getId() {
+        return id;
+    }
+
+    @Column(name = "txt")
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "next_message_id")
+    public Message getNextMessage() {
+        return nextMessage;
+    }
+
+    public void setNextMessage(Message nextMessage) {
+        this.nextMessage = nextMessage;
+    }
+}
